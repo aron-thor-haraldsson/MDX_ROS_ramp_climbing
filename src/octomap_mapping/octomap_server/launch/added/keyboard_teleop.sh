55ea6e1787fb83b7e90adba2d07a87369223829f
AH1534 <ah1534@live.mdx.ac.uk>
2018-01-29 14:52:59 +0000

    added custom launch files and moved one that was created previously so that they do not mix with the original development package

---
+++ src/octomap_mapping/octomap_server/launch/added/keyboard_teleop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+
+gnome-terminal -x bash -c "roslaunch turtlebot_teleop keyboard_teleop.launch" 
+
+
+
+
+
+#while :
+#do
+#    sleep $MAP_UPDATE_INTERVAL
+#    #rosrun map_server map_saver -f $MAP_FILE_NAME
+#done
